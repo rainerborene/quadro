f6552bae1424ca6d8355b2d593b3db8731c8f336
Rainer Borene <rainerborene@gmail.com>
2011-08-04 16:13:14 -0300

    stylesheets isn't writable on Heroku

--- config/compass.rb
+++ config/compass.rb
@@ -5,6 +5,10 @@ project_type = :rails
 # Set this to the root of your project when deployed:
 http_path = "/"
 
+# Changes directory path to make Compass work on Heroku
+css_dir = "tmp/stylesheets"
+sass_dir = "app/views/stylesheets"
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
