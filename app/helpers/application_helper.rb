51534bb84f6c382a0a92c97a9a627c87acdd8239
Rainer Borene <rainerborene@gmail.com>
2011-10-02 16:35:20 -0300

    Added Google Analytics tracking code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def current_user_session
-    output = { :authenticated => signed_in?, }
+    output = { :authenticated => signed_in? }
     output.merge!(current_user.serializable_hash) if signed_in?
     output.to_json.html_safe
   end
