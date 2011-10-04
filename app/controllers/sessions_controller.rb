f3094c2a9e1a49c318ed2dce39a663448cef09d0
Rainer Borene <rainerborene@gmail.com>
2011-10-03 22:56:06 -0300

    Fixed condition for sending tweet message.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,19 +2,18 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.new
-
-    if user.new? and ENV["SOCIAL_MESSENGER"] == "yes"
-      credentials = auth["credentials"]
-      message = "I'm using @quadroapp to store post-its on the cloud."
-      Delayed::Job.enqueue TweetingJob.new(message, credentials["token"], credentials["secret"])
-    end
-
     user.update_with_omniauth(auth)
     session[:user_id] = user.id.to_s
 
     if user.boards.empty?
       user.boards.create!(:title => "Untitled")
       user.save
+    end
+
+    if user.token.blank? and ENV["SOCIAL_MESSENGER"] == "yes"
+      credentials = auth["credentials"]
+      message = "I'm using @quadroapp to store post-its on the cloud."
+      Delayed::Job.enqueue TweetingJob.new(message, credentials["token"], credentials["secret"])
     end
 
     redirect_to root_url
