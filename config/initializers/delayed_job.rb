8bdcb1828c37424a55248f19e4a9435777b56893
Rainer Borene <rainerborene@gmail.com>
2011-10-05 15:10:32 -0300

    Revert "Enable destroy failed jobs option"
    This reverts commit 300b35a4d1d3b90c4cb2f5e3128255c1400cb242.

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,6 +1,6 @@
 require "twitter_job"
 
 Delayed::Worker.backend = :mongo_mapper
-Delayed::Worker.destroy_failed_jobs = true
+Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_run_time = 5.minutes
 Delayed::Worker.delay_jobs = !Rails.env.test?
