36b86f5fc665b7cf69ef1175686b2e598115fcdb
Rainer Borene <rainerborene@gmail.com>
2011-10-23 15:48:40 -0200

    Configure MongoMapper generators.

--- config/application.rb
+++ config/application.rb
@@ -37,6 +37,9 @@ module Quadro
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Configure MongoMapper generators
+    config.generators.orm = :mongo_mapper
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
